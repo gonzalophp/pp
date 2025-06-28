1d6e76efb1e6b4457a7b9aa30b30e1351f78c169
gonzalo.gradovivero <gonzalo.gradovivero@awin.com>
2025-06-28 12:10:47 +0200

    Added composer.lock

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context): Kernel {
     // var_export($_SERVER);
     $context['APP_ENV'] = 'dev';
-    $context['APP_DEBUG'] = false;
+    $context['APP_DEBUG'] = true;
     // exit;
     // var_export(['context' => $context]);
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
